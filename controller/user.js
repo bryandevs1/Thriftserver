cd7fc5f4d022ed991fe9b6231d1b846b60359773
Bryan <bryanojji4@gmail.com>
2024-10-03 14:15:23 +0100

    dd

--- controller/user.js
+++ controller/user.js
@@ -35,7 +35,7 @@ router.post("/create-user", async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `https://bryandevs1.vercel.app/activation/${activationToken}`;
+    const activationUrl = `https://thriftserver.vercel.app/activation/${activationToken}`;
 
     try {
       await sendMail({
