5dcd5a43b3a1013bd49274a15acf71f4ed8e10cc
Bryan <bryanojji4@gmail.com>
2024-10-03 14:22:28 +0100

    dd

--- controller/user.js
+++ controller/user.js
@@ -97,6 +97,43 @@ router.post(
     }
   })
 );
+// activate user
+router.get(
+  "/activation/:activation_token",
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const { activation_token } = req.params;
+
+      const newUser = jwt.verify(
+        activation_token,
+        process.env.ACTIVATION_SECRET
+      );
+
+      if (!newUser) {
+        return next(new ErrorHandler("Invalid token", 400));
+      }
+
+      const { name, email, password, avatar } = newUser;
+
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return next(new ErrorHandler("User already exists", 400));
+      }
+
+      user = await User.create({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      sendToken(user, 201, res);
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  })
+);
 
 // login user
 router.post(
